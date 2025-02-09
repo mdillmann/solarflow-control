3eeea14efdc607e961c686d1baf2f6c2c4aecf42
Markus Dillmann <markus.dillmann@gmx.net>
2025-02-09 16:04:21 +0100

    adjustments for local build

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
 branch=`git rev-parse --abbrev-ref HEAD`
 docker build -t mdillman666/solarflow-control:$branch .
 
-#docker image push mdillman666/solarflow-control:$branch
+docker image push mdillman666/solarflow-control:$branch
